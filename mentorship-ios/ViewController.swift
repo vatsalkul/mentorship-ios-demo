01090e77c9485eef7b3245ca311ef94608259a22
vatsalkul <vatsalkulshreshtha@gmail.com>
2020-02-21 00:14:25 +0530

    Initial Commit

---
+++ mentorship-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mentorship-ios
+//
+//  Created by Vatsal Kulshreshtha on 21/02/20.
+//  Copyright © 2020 Vatsal Kulshreshtha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
